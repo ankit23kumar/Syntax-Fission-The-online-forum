47c63d89dde340847ae0677aa415f193eb5ebad0
ankit23kumar <ankitkumar231002@gmail.com>
2025-07-19 13:17:22 +0530

    Just change readme file

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,7 +5,7 @@ import Login from '../pages/Login'
 import Register from '../pages/Register'
 import RegisterStep2 from '../pages/Register2'
 // import Dashboard from '../pages/Dashboard'
-import Profile from '../pages/Profile'
+import UserDashboardProfile from '../pages/UserDashboardProfile'
 import NewQuestions from '../pages/NewQuestions'
 import AskQuestion from '../pages/AskQuestion'
 import QuestionView from '../pages/QuestionView'
@@ -17,7 +17,7 @@ const AppRoutes = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/register/step-2" element={<RegisterStep2 />} />
-      <Route path="/profile" element={<Profile />} /> 
+      <Route path="/profile" element={<UserDashboardProfile />} /> 
       {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       <Route path="/new-questions" element={<NewQuestions />} />
       <Route path="/ask-question" element={<AskQuestion/>} />
